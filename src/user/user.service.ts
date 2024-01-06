50835ba8f769386b3e4ec64b278ad438a642564f
amito07 <lianbadhon@gmail.com>
2024-01-06 13:54:12 +0600

    feat: add controller basic

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,7 +2,15 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class UserService {
-    findOne(): string{
-        return `Hello Bangladesh !`
+    findOne(id): string{
+        return `Hello Bangladesh ${id}!`
+    }
+
+    basic(): string{
+        return 'Working Perfectly !'
+    }
+
+    create(name): string{
+        return `User created successfully name ${name} !`
     }
 }
