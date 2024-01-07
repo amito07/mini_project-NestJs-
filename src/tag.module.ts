d82f38f7c73fa99ed476393ab29106479ef73f07
amito07 <lianbadhon@gmail.com>
2024-01-07 13:52:16 +0600

    feat: add global module

--- src/tag.module.ts
+++ src/tag.module.ts
@@ -1,10 +1,12 @@
 import { Module } from "@nestjs/common";
 import { TagController } from "./tag.controller";
 import { TagService } from "./tag.service";
+import { UserModule } from "./user/user.module";
+import { UserService } from "./user/user.service";
 
 @Module({
     controllers:[TagController],
-    providers:[TagService]
+    providers:[TagService, UserService]
 })
 
 export class TagModule {}
